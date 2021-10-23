1098eef1a94c7bc7b48299541f8c2c763ebe7140
Andrew <andrewmgunn26@gmail.com>
2021-10-23 18:45:37 -0500

    Initial commit

---
+++ WindowsUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
